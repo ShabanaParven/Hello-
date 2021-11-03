92cf5e36f9921d8db2a2f06a01db5e541fb1c2d3
ShabanaParven <92175723+ShabanaParven@users.noreply.github.com>
2021-11-03 00:49:38 -0700

    Update LeapYear.cs

--- BasicLogicalProgram/LeapYear.cs
+++ BasicLogicalProgram/LeapYear.cs
@@ -15,7 +15,7 @@ namespace BasicLogicalProgram
             Console.WriteLine("Enter the year");
             year = Convert.ToInt32(Console.ReadLine());
 
-            if((year % 4 == 0) && (year % 100 != 0 || (year % 400 == 0)))
+            if((year % 400 == 0) || (year % 4 == 0) && (year % 100 != 0))
             {
                 Console.WriteLine("its as LeapYear year");
             }
