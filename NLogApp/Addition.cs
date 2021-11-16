2cdfe03c578d93dc249a412ce9c3db262f20e828
Shabana <shabanaparveen487@gmail.com>
2021-11-15 23:37:38 -0800

    NLog Applications

---
+++ NLogApp/Addition.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLogApp
+{
+    public class Addition
+    {
+        static NLog nLog = new NLog();
+        public static int Add(int firstNumber, int secodNumber)
+        {
+            int sum = 0;
+            try
+            {
+                if (firstNumber == 0 || secodNumber == 0)
+                {
+                    nLog.LogDebug("debug successfully");
+                    nLog.LogError("First number or second number is zero");
+                    nLog.LogInfo("Add method");
+                    return 0;
+                }
+                sum = firstNumber + secodNumber;
+                nLog.LogInfo("Additio method executed");
+                nLog.LogDebug("Debug successfully");
+                nLog.LogWarn("first number or second number is not equal to zero");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return sum;
+
+        }
+        
+    }
+}
---
+++ NLogApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLogApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcom to NLogApp program");
+            Addition.Add(2, 3);
+            Console.ReadKey();
+
+        }
+    }
+}
