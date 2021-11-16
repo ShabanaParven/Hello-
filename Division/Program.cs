f5bd4f29787d57cdc47b5f2c3cd6324375fbeb92
ShabanaParven <92175723+ShabanaParven@users.noreply.github.com>
2021-11-16 22:55:33 +0530

    Division

---
+++ Division/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Division
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Div ojcDiv = new Div();
+            Console.WriteLine("total values="+ ojcDiv.isDivision(10, 5));
+            Console.ReadKey();
+        }
+    }
+}
