13ae482f59e39fd19924843bac824cb2b34e7b92
ShabanaParven <92175723+ShabanaParven@users.noreply.github.com>
2021-11-16 23:13:07 +0530

    Multiplication

---
+++ Multiplication/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplication
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Mul ojcSub = new Mul();
+            Console.WriteLine("total values=" + ojcSub.isdif(5, 10));
+            Console.ReadKey();
+        }
+    }
+}
