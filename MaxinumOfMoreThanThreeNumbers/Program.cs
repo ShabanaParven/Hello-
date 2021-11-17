628d229132f02d3575344a3981d8b8b38acc2311
Shabana <shabanaparveen487@gmail.com>
2021-11-17 10:39:28 -0800

    Maximun of more than three numbers

---
+++ MaxinumOfMoreThanThreeNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxinumOfMoreThanThreeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MaxNumber.maxNumber();
+            Console.ReadKey();
+        }
+    }
+}
