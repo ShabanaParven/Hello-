b23058ede319abe0e8efcbde609b682636a088b3
Shabana <shabanaparveen487@gmail.com>
2021-11-17 10:52:38 -0800

    maximum number

---
+++ MaximumGenericThreeVariablesNo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MaximumGenericThreeVariablesMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {  54, 89, 45 };
+            GenericMaximum<int> generic = new GenericMaximum<int>(arr);
+            generic.PrintMaxValue();
+            Console.ReadKey();
+        }
+    }
+}
