93bee824a2469ffc9fc08f0463c949932b7b8fd6
Shabana <shabanaparveen487@gmail.com>
2021-11-18 10:35:51 -0800

    Delete last element from list

---
+++ DeleteLastElementFromList/LinkedList.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeleteLastElementFromList
+{
+    public class LinkedList
+    {
+        private int item;
+        private LinkedList next;
+
+        public LinkedList(int value)
+        {
+            item = value;
+            next = null;
+        }
+        public LinkedList AddItem(int value)
+        {
+            LinkedList node = new LinkedList(value);
+            if (this.next == null)
+            {
+                node.next = null;
+                this.next = node;
+            }
+            else
+            {
+                LinkedList temp = this.next;
+                node.next = temp;
+                this.next = node;
+            }
+            return node;
+        }
+        public void ListTraverse()
+        {
+            LinkedList node = this;
+
+            while (node != null)
+            {
+                Console.WriteLine("-->" + node.item);
+                node = node.next;
+            }
+        }
+
+        public void DeleteNode(LinkedList given)
+        {
+            LinkedList node = this;
+
+            while (node != null)
+            {
+                if (node.next == given)
+                {
+                    node.next = given.next;
+                    break;
+                }
+                node = node.next;
+            }
+        }
+    }
+}
---
+++ DeleteLastElementFromList/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeleteLastElementFromList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList StartNode = new LinkedList(56);
+
+            LinkedList n1;
+            LinkedList n2;
+
+            n1 = StartNode.AddItem(30);
+            n2 = n1.AddItem(70);
+
+            Console.WriteLine("List before deletion:");
+            StartNode.ListTraverse();
+
+            StartNode.DeleteNode(n2);
+            Console.WriteLine("List after deletion of last element:");
+            StartNode.ListTraverse();
+
+            Console.ReadKey();
+        }
+        
+    }
+}
