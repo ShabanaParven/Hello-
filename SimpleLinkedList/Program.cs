93fc7079965ba115c6f937115b33877ca427aa03
Shabana <shabanaparveen487@gmail.com>
2021-11-18 10:22:38 -0800

    Create simple linkedlist

---
+++ SimpleLinkedList/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+
+            Console.ReadKey();
+        }
+    }
+}
