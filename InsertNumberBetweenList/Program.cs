f8c55e5b0ce519a558efaed3aa13a9cccf16e8fa
Shabana <shabanaparveen487@gmail.com>
2021-11-18 10:32:44 -0800

    Inser number between list

---
+++ InsertNumberBetweenList/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertNumberBetweenList
+{
+   public  class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            Console.WriteLine("before adding");
+            list.Add(56);
+            //list.Add(30);
+            list.Add(70);
+            list.Display();
+            Console.WriteLine("--------------");
+            Console.WriteLine("After adding");
+            list.InsertAtPerticularPosition(2, 30);
+            list.Display();
+
+            Console.ReadKey();
+        }
+    }
+}
