838b07847e9c9672d36f953709e26a7a38c644e8
Shabana <shabanaparveen487@gmail.com>
2021-11-18 10:08:12 -0800

    Create queue using linkedlist

---
+++ QueueUsingLinkedList/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueUsingLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListQueue list = new LinkedListQueue();
+            list.Enqueue(56);
+            list.Enqueue(30);
+            list.Enqueue(70);
+            list.Display();
+
+            Console.ReadKey();
+        }
+    }
+}
