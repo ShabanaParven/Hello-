b493a6b34436daeb7c582b4fbe67df0d2dc68ca9
Shabana <shabanaparveen487@gmail.com>
2021-11-18 10:11:59 -0800

    Queue using linkedlist

---
+++ PerformDequeue/LinkedListQueue.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerformDequeue
+{
+    public class LinkedListQueue
+    {
+        Node head = null;
+
+        internal void Enqueue(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+
+            }
+            Console.WriteLine("{0} inserted into queue", node.data);
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+        internal void Dequeue(int data)
+        {
+            if (this.head == null)
+            {
+                Console.WriteLine("Stack is empty, deletion is not possible");
+                return;
+            }
+            Console.WriteLine("value deleted {0} ", this.head.data);
+            this.head = this.head.next;
+        }
+    }
+}
---
+++ PerformDequeue/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerformDequeue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListQueue list = new LinkedListQueue();
+            list.Enqueue(56);
+            list.Enqueue(30);
+            list.Enqueue(70);
+            list.Display();
+            list.Dequeue(56);
+            list.Dequeue(30);
+            list.Display();
+
+            Console.ReadKey();
+        }
+    }
+}
