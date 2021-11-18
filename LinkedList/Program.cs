79c029b8d3d532bfa63b707948c0e612decd0a49
Shabana <shabanaparveen487@gmail.com>
2021-11-18 10:20:01 -0800

    create a linkedlist

---
+++ LinkedList/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+
+            foreach(int item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
