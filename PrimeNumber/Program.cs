613b6357b6373a980ea4de33adcf01b88132e511
Shabana <shabanaparveen487@gmail.com>
2021-11-24 10:56:34 -0800

    find prime number

---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int num, i, ctr, strno, eeno;
+
+            strno = 0;
+            eeno = 1000;
+
+            Console.WriteLine("The prime numbers between {0} and {1} are : \n", strno, eeno);
+
+            for (num = strno; num <= eeno; num++)
+            {
+                ctr = 0;
+
+                for (i = 2; i <= num / 2; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        ctr++;
+                        break;
+                    }
+                }
+
+                if (ctr == 0 && num != 1)
+                {
+                    Console.Write("{0}\n", num);
+                }
+
+            }
+            Console.Write("\n");
+
+        }
+    }
+}
