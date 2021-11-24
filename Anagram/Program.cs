59093f02412ac95f86fb6035b9328f6506e54169
Shabana <shabanaparveen487@gmail.com>
2021-11-24 10:43:10 -0800

    Two strings are anagram or not

---
+++ Anagram/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "shabana";
+            string str2 = "parveen";
+
+            char[] ch1 = str1.ToLower().ToCharArray();
+            char[] ch2 = str2.ToLower().ToCharArray();
+
+            Array.Sort(ch1);
+            Array.Sort(ch2);
+
+            string val1 = new string(ch1);
+            string val2 = new string(ch2);
+
+            if(val1 == val2 )
+            {
+                Console.WriteLine("Anagram");
+
+            }
+            else
+            {
+                Console.WriteLine("Not an Anagram");
+            }
+            Console.ReadKey();
+        }
+    }
+}
