d12c55a6a01479a25e06947323f580af12b4b0b8
Shabana <shabanaparveen487@gmail.com>
2021-11-27 08:46:26 -0800

    Check for alid first name

---
+++ UserRegistrationUC1/Patterns.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationUC1
+{
+    public class Patterns
+    {
+        public static string REGEX_FIRSTNAME = "^[A-Z][a-z]{2}$";
+
+        public bool validFirstName(string firstName)
+        
+        {
+           
+            return Regex.IsMatch(firstName, REGEX_FIRSTNAME);
+        }
+    }
+}
---
+++ UserRegistrationUC1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationUC1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool checkvalue;
+            string patt = "Sha";
+            Patterns patterns = new Patterns();
+            checkvalue = patterns.validFirstName(patt);
+            if (checkvalue)
+            {
+                Console.WriteLine("Pattern matched");
+            }
+            else
+            {
+                Console.WriteLine("Pattern Not matched");
+            }
+            Console.ReadKey();
+        }
+    }
+}
