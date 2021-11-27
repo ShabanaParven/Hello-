4d9ad979cd58075544bcde340a30de0d07a62c97
Shabana <shabanaparveen487@gmail.com>
2021-11-27 09:54:20 -0800

    check email pattern

---
+++ UserRegistrationUC9/Pattern.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationUC9
+{
+   public class Pattern
+    {
+        public static string REGEX_EMAIL = "^[a][b][c][@][1][.][c][o][m]$";
+
+        public bool validEmail(string email)
+        {
+            return Regex.IsMatch(email, REGEX_EMAIL);
+        }
+    }
+}
---
+++ UserRegistrationUC9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationUC9
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            bool checkvalue;
+            string patt = "abc@1.com";
+            Pattern patterns = new Pattern();
+            checkvalue = patterns.validEmail(patt);
+            if (checkvalue)
+            {
+                Console.WriteLine("pattern matched");
+                Console.WriteLine("Clear email sample");
+            }
+            else
+            {
+                Console.WriteLine("No matched");
+            }
+            Console.ReadKey();
+        }
+    }
+}
