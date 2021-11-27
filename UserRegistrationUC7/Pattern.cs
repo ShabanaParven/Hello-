c9c5de095185ac2b14153b224304e518e55fcd75
Shabana <shabanaparveen487@gmail.com>
2021-11-27 09:04:51 -0800

    Rule3 to creat password

---
+++ UserRegistrationUC7/Pattern.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationUC7
+{
+    public class Pattern
+    {
+        public static string REGEX_PASSWORD = "^[A-Z]{1}[0-9][a-z]{6}$";
+
+        public bool validPassword(string password)
+        {
+            return Regex.IsMatch(password, REGEX_PASSWORD);
+        }
+    }
+}
