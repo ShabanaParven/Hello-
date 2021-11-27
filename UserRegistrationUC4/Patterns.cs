370c29f773e06f6bd09aa21173b8cae63c4e4230
Shabana <shabanaparveen487@gmail.com>
2021-11-27 08:56:15 -0800

    Folllow pre defined mobile format

---
+++ UserRegistrationUC4/Patterns.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationUC4
+{
+    public class Patterns
+    {
+        public static string REGEX_MOBNUMBER = "^[+][9][1] [6-9]{1}[0-9]{9}$";
+
+        public bool validMobNum(string mobNum)
+        {
+            return Regex.IsMatch(mobNum, REGEX_MOBNUMBER);
+        }
+    }
+}
---
+++ UserRegistrationUC4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationUC4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool checkvalue;
+            string patt = "+91 6360132491";
+            Patterns patterns = new Patterns();
+            checkvalue = patterns.validMobNum(patt);
+            if (checkvalue)
+            {
+                Console.WriteLine("pattern matched");
+            }
+            else
+            {
+                Console.WriteLine("No matched");
+            }
+            Console.ReadKey();
+        }
+    }
+}
