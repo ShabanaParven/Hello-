db500cf24d5d2c7624c155ca89242c2cba05aba2
Shabana <shabanaparveen487@gmail.com>
2021-11-27 08:50:09 -0800

    Check for valid last name

---
+++ UserRegistrationUC2/Patterns.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationUC2
+{
+    public class Patterns
+    {
+        public static string REGEX_LASTNAME = "^[A-Z][a-z]{2}$";
+
+        public bool validLastName(string lastName)
+        {
+            return Regex.IsMatch(lastName, REGEX_LASTNAME);
+        }
+    }
+}
---
+++ UserRegistrationUC2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationUC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool checkvalue;
+            string patt = "Rao";
+            Patterns patterns = new Patterns();
+            checkvalue = patterns.validLastName(patt);
+            if (checkvalue)
+            {
+                Console.WriteLine("pattern matched");
+            }
+            else
+            {
+                Console.WriteLine("No matched");
+            }
+            Console.ReadKey();
+        }
+    }
+}
