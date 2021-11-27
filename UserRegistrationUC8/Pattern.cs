63dc8b7142b2bf578deb01e0ddc03ca7f8f869f4
Shabana <shabanaparveen487@gmail.com>
2021-11-27 09:08:04 -0800

    Rule4 with all above rules for password

---
+++ UserRegistrationUC8/Pattern.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationUC8
+{
+    public class Pattern
+    {
+        public static string REGEX_PASSWORD = "^[A-Z]{1}[0-9]{1}[@][a-z]{5}$";
+
+        public bool validPassword(string password)
+        {
+            return Regex.IsMatch(password, REGEX_PASSWORD);
+        }
+    }
+}
---
+++ UserRegistrationUC8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationUC8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool checkvalue;
+            string patt = "K5@ehytv";
+            Pattern pattern = new Pattern();
+            checkvalue = pattern.validPassword(patt);
+            if (checkvalue)
+            {
+                Console.WriteLine("pattern matched");
+            }
+            else
+            {
+                Console.WriteLine("No matched");
+            }
+            Console.ReadKey();
+        }
+    }
+}
