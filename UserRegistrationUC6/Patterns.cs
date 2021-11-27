72eb204f7dc32c66e1724df9a22b7a8142e57d2b
Shabana <shabanaparveen487@gmail.com>
2021-11-27 09:02:42 -0800

    Rule2 to create password

---
+++ UserRegistrationUC6/Patterns.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationUC6
+{
+    public class Patterns
+    {
+        public static string REGEX_PASSWORD = "^[A-Z]{1}[a-z]{7}$";
+
+        public bool validPassword(string password)
+        {
+            return Regex.IsMatch(password, REGEX_PASSWORD);
+        }
+    }
+}
---
+++ UserRegistrationUC6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationUC6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool checkvalue;
+            string patt = "Acadamic";
+            Patterns patterns = new Patterns();
+            checkvalue = patterns.validPassword(patt);
+            if (checkvalue)
+            {
+                Console.WriteLine("pattern matched");
+            }
+            else
+            {
+                Console.WriteLine("No matched");
+            }
+            Console.ReadKey();
+        }
+    }
+}
