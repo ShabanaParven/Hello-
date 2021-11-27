88aac44cc016beeb08c91cfd9b82594c989d5dba
Shabana <shabanaparveen487@gmail.com>
2021-11-27 08:59:06 -0800

    Rule1 to creat password

---
+++ UserRegistrationUC5/Pattern.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationUC5
+{
+    public class Pattern
+    {
+        public static string REGEX_PASSWORD = "^[a-z]{8}$";
+
+        public bool validPassword(string password)
+        {
+            return Regex.IsMatch(password, REGEX_PASSWORD);
+        }
+    }
+}
---
+++ UserRegistrationUC5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationUC5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool checkvalue;
+            string patt = "publishh";
+            Pattern pattern = new Pattern();
+            checkvalue = pattern.validPassword(patt);
+            if (checkvalue)
+            {
+                Console.WriteLine("pattern matched");
+            }
+            else
+            {
+                Console.WriteLine("No matched");
+            }
+            Console.ReadKey();
+        }
+    }
+}
