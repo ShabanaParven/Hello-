9c9f6b1c79fe4bb39bd05724223ceb0317fd34f3
Shabana <shabanaparveen487@gmail.com>
2021-11-27 08:53:29 -0800

    Check for valid mail

---
+++ UserRegistrationUC3/Patterns.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationUC3
+{
+    public class Patterns
+    {
+        public static string REGEX_Email = "^[a][b][c][.][a-z]{3}[@][b][l][.][c][o][.][a-z]{2}$";
+
+        public bool validEmail(string email)
+        {
+            return Regex.IsMatch(email, REGEX_Email);
+        }
+    }
+}
---
+++ UserRegistrationUC3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationUC3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool checkvalue;
+            string patt = "abc.sha@bl.co.in";
+            Patterns patterns = new Patterns();
+            checkvalue = patterns.validEmail(patt);
+            if (checkvalue)
+            {
+                Console.WriteLine("pattern matched");
+            }
+            else
+            {
+                Console.WriteLine("No matched");
+            }
+            Console.ReadKey();
+        }
+    }
+}
