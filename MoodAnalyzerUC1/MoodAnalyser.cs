bb4922194e3c28513761b23f5d4810c80d0b8c26
Shabana <shabanaparveen487@gmail.com>
2021-11-29 09:39:56 -0800

    Ability to analyze the respond happy or sad mood

---
+++ MoodAnalyzerUC1/MoodAnalyser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerUC1
+{
+    public class MoodAnalyser
+    {
+        private string message;
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+        public string AnalyserMood()
+        {
+            if (this.message.Contains("Sad"))
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "Happy";
+            }
+        }
+       
+    }
+}
