8733ba836cd56484888337f7e2a608420737815e
Shabana <shabanaparveen487@gmail.com>
2021-11-29 09:43:10 -0800

    Handle exception if user provide invalid mood

---
+++ MoodAnalyserUC2/MoodAnalyser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserUC2
+{
+    public class MoodAnalyser
+    {
+        private string message;
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+        public string AnalyserMood()
+        {
+            try
+            {
+                if(this.message.Contains("Sad"))
+                {
+                    return "SAD";
+                }
+                else
+                {
+                    return "HAPPY";
+                }
+            }
+            catch
+            {
+                return "HAPPY";
+            }
+        }
+        
+    }
+}
