de285a8324489333d125a6fe4e47a39c63c1afea
Shabana <shabanaparveen487@gmail.com>
2021-12-01 08:07:22 -0800

    Mood analyser with default constructor

---
+++ MoodAnalyserUC4/MoodAnalyserCustomException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserUC4
+{
+    public class MoodAnalyserCustomException:Exception
+    {
+        public enum ExceptionType
+        {
+            NULL_MESSAGE, EMPTY_MESSAGE,
+            NO_SUCH_FIELD, NO_SUCH_METHOD,
+            NO_SUCH_CLASS, OBJECT_CREATION_ISSUES
+        }
+
+        public readonly ExceptionType type;
+
+        public MoodAnalyserCustomException(ExceptionType Type, string message) : base(message)
+        {
+            this.type = Type;
+        }
+    }
+}
