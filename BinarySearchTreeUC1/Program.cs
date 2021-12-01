d0d29e50910d8682256b145e9984079eaad29d77
Shabana <shabanaparveen487@gmail.com>
2021-12-01 08:09:58 -0800

    Create a BST by adding elements

---
+++ BinarySearchTreeUC1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTreeUC1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Binary Search Tree Program.");
+            BinaryTree binarytree = new BinaryTree();
+
+            binarytree.Add(56);
+            binarytree.Add(30);
+            binarytree.Add(70);
+
+            Console.Write("\n Inorder result : ");
+            binarytree.TraverseInorder(binarytree.root);
+            Console.WriteLine();
+
+            Console.Write("\n Post-order result : ");
+            binarytree.TraversePostorder(binarytree.root);
+            Console.WriteLine();
+
+            Console.Write("\n Pre-order result : ");
+            binarytree.TraversePreorder(binarytree.root);
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+        
+    }
+}
