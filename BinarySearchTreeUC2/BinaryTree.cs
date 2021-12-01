7a66dc5409d68204bc9bd07f614fe786baa66d54
Shabana <shabanaparveen487@gmail.com>
2021-12-01 08:12:24 -0800

    Create BST

---
+++ BinarySearchTreeUC2/BinaryTree.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTreeUC2
+{
+    public class BinaryTree
+    {
+        public Node root = null;
+
+        public void Add(int i)
+        {
+            Node node = new Node();
+            node.data = i;
+            if (root == null)
+            {
+                root = node;
+            }
+            else
+            {
+                Node current = root;
+                Node parent;
+                while (true)
+                {
+                    parent = current;
+                    if (i < current.data)
+                    {
+                        current = current.LeftNode;
+                        if (current == null)
+                        {
+                            parent.LeftNode = node;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        current = current.RightNode;
+                        if (current == null)
+                        {
+                            parent.RightNode = node;
+                            break;
+                        }
+                    }
+
+                }
+            }
+            Console.WriteLine(node.data + " : Data is added");
+        }
+
+        public void Inorder(Node root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            Inorder(root.LeftNode);
+            Console.Write(root.data + " ");
+            Inorder(root.RightNode);
+
+        }
+        public void Preorder(Node root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+            Console.Write(root.data + " ");
+            Preorder(root.LeftNode);
+
+            Preorder(root.RightNode);
+
+        }
+        public void Postorder(Node root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            Postorder(root.LeftNode);
+
+            Postorder(root.RightNode);
+            Console.Write(root.data + " ");
+        }
+    }
+}
---
+++ BinarySearchTreeUC2/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTreeUC2
+{
+    public class Node
+    {
+        public int data;
+        public Node LeftNode;
+        public Node RightNode;
+        public Node()
+        {
+            this.LeftNode = null;
+            this.RightNode = null;
+        }
+    }
+}
